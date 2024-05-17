e62143138a5d01d60eaceb214f12d4bcfb1705d4
Pujan Paudel <132932259+Paudel002@users.noreply.github.com>
2024-05-17 16:57:57 +0000

    first code

---
+++ .devcontainer/01.variable.js
@@ -0,0 +1,17 @@
+const accountID = 12233
+let accountEmail = "pujan@google.com"
+var accountPassword ="12345"
+accountCity = "pokhara"
+let accountstate;
+
+
+accountEmail = "pu@pu.com"
+accountPassword = "122334"
+accountCity = "kathamandu"
+
+// accountId = 2
+
+console.log(accountID);
+
+// 
+console.table([accountID,accountEmail,accountPassword,accountCity,accountstate])
